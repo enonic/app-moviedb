5e5398e7144fc9f7684ba71ff557c62103f8280f
Thomas Sigdestad <tsi@enonic.com>
2023-03-17 15:19:16 +0100

    removed unneeded logging

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -83,17 +83,8 @@ function createContent() {
         },
         includeNodeIds: true
     });
-    log.info('-------------------');
-    log.info('Imported nodes:');
-    importNodes.addedNodes.forEach(element => log.info(element));
-    log.info('-------------------');
-    log.info('Updated nodes:');
-    importNodes.updatedNodes.forEach(element => log.info(element));
-    log.info('-------------------');
-    log.info('Imported binaries:');
-    importNodes.importedBinaries.forEach(element => log.info(element));
-    log.info('-------------------');
-    if (importNodes.importErrors.length !== 0) {
+    log.info('Importing MovieDB sample content');
+    if (importNodes.importErrors.length > 0) {
         log.warning('Errors:');
         importNodes.importErrors.forEach(element => log.warning(element.message));
         log.info('-------------------');
@@ -106,20 +97,7 @@ function publishRoot() {
         sourceBranch: 'draft',
         targetBranch: 'master',
     });
-    if (result) {
-        log.info('-------------------');
-        log.info('Published nodes:\n');
-        result.pushedContents.forEach(element => log.info(element));
-        log.info('-------------------');
-        log.info('Deleted nodes:\n');
-        result.deletedContents.forEach(element => log.info(element));
-        log.info('-------------------');
-        if (result.failedContents) {
-            log.warning('Failed nodes:\n');
-            result.failedContents.forEach(element => log.warning(element));
-            log.info('-------------------');
-        }
-    } else {
+    if (!result) {
         log.warning('Could not publish imported content.');
     }
 }
