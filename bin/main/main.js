8a70fc737044813b907b2436c4a09efc8d65c616
Thomas Sigdestad <tsi@enonic.com>
2023-04-13 22:11:04 +0200

    removed bin folder

--- bin/main/main.js
+++ bin/main/main.js
@@ -6,10 +6,16 @@ const projectLib = require('/lib/xp/project');
 const taskLib = require('/lib/xp/task');
 
 const projectData = {
-    id: 'hmdb',
-    displayName: 'Headless Demo',
-    description: 'Headless Movie DB sample content',
+    id: 'moviedb',
+    displayName: 'Movie DB',
+    description: 'Sample content from the cinematic industry',
     language: 'en',
+    siteConfig: [{
+        applicationKey: app.name,
+        config: {
+            a: 'b'
+        }
+    }],
     readAccess: {
         public: true
     }
@@ -44,7 +50,7 @@ const initialize = function () {
         const project = getProject();
         if (!project) {
             taskLib.executeFunction({
-                description: 'Importing HMDB content',
+                description: 'Importing Movie DB content',
                 func: initProject
             });
         }
@@ -63,7 +69,7 @@ const initProject = function () {
         createContent();
         publishRoot();
     } else {
-        log.error('Project "' + projectData.id + '" failed to be created');
+        log.error('Project "' + projectData.id + '" create failed');
     }
 };
 
@@ -77,17 +83,8 @@ function createContent() {
         },
         includeNodeIds: true
     });
-    log.info('-------------------');
-    log.info('Imported nodes:');
-    importNodes.addedNodes.forEach(element => log.info(element));
-    log.info('-------------------');
-    log.info('Updated nodes:');
-    importNodes.updatedNodes.forEach(element => log.info(element));
-    log.info('-------------------');
-    log.info('Imported binaries:');
-    importNodes.importedBinaries.forEach(element => log.info(element));
-    log.info('-------------------');
-    if (importNodes.importErrors.length !== 0) {
+    log.info('Importing MovieDB sample content');
+    if (importNodes.importErrors.length > 0) {
         log.warning('Errors:');
         importNodes.importErrors.forEach(element => log.warning(element.message));
         log.info('-------------------');
@@ -96,24 +93,11 @@ function createContent() {
 
 function publishRoot() {
     const result = contentLib.publish({
-        keys: ['/hmdb'],
+        keys: ['/movies', '/persons','/articles', '/playlists' ],
         sourceBranch: 'draft',
         targetBranch: 'master',
     });
-    if (result) {
-        log.info('-------------------');
-        log.info('Published nodes:\n');
-        result.pushedContents.forEach(element => log.info(element));
-        log.info('-------------------');
-        log.info('Deleted nodes:\n');
-        result.deletedContents.forEach(element => log.info(element));
-        log.info('-------------------');
-        if (result.failedContents) {
-            log.warning('Failed nodes:\n');
-            result.failedContents.forEach(element => log.warning(element));
-            log.info('-------------------');
-        }
-    } else {
+    if (!result) {
         log.warning('Could not publish imported content.');
     }
 }
